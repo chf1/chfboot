26cbd23e0caed8314fe0d86d24315708a5bfae8b
chf1 <chris-f@gmx.de>
2021-11-18 21:37:13 +0100

    foo

--- src/main/java/de/chf/demo/Rest.java
+++ src/main/java/de/chf/demo/Rest.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class Rest {
 
 	private static final String template = "Hello, %s!";
 
